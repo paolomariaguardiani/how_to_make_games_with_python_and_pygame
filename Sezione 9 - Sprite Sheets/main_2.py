3d4ab70599a432590ee28bb77ba4fb4e41393904
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2024-08-18 17:11:32 +0200

    Sezione 9 finished - Course finished

---
+++ Sezione 9 - Sprite Sheets/main_2.py
@@ -0,0 +1,68 @@
+import pygame
+import random
+# from pygame.locals import *
+
+pygame.init()
+
+# sidplay will be a surface
+display = pygame.display.set_mode([840, 480])
+pygame.display.set_caption("My first Game!")
+
+# Group:
+logicGroup = pygame.sprite.Group()
+visibleGroup = pygame.sprite.Group()
+
+
+class Character(pygame.sprite.Sprite):
+    # *group perché lo sprite può appartenere a più gruppi contemporaneamente
+    def __init__(self, *group): # 
+        super().__init__(*group)
+        self.rect = pygame.Rect(200,200,100,100)
+
+        # We specify how many cells the spritesheet has
+        self.cells = [9, 5] # 9 columns and 5 rows        
+        self.spritesheet = pygame.image.load("data/charTileset.png")
+        self.spritesheet = pygame.transform.scale(
+            self.spritesheet,
+            [self.rect.width * self.cells[0], self.rect.height * self.cells[1]]
+        )
+        print(self.rect.width)
+
+        self.image = pygame.Surface(self.rect.size, pygame.SRCALPHA)
+        self.setTile(1, 1)
+
+    def setTile(self, x, y):
+        self.image.fill([0, 0, 0, 0])
+        # The value must be negative!!!
+        self.image.blit(self.spritesheet, [-self.rect.w * x, -self.rect.h * y])
+
+    # We override the update method:
+    def update(self):
+        pass
+
+
+# Sprites:
+char1 = Character(visibleGroup, logicGroup)
+char1.rect.center = [150, 150]
+
+clock = pygame.time.Clock()
+gameloop = True
+while gameloop:
+    clock.tick(60) # 60 frames per second
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameloop = False
+        elif event.type == pygame.KEYDOWN: # KEYUP
+            if event.key == pygame.K_ESCAPE:
+                gameloop = False
+
+    ####
+    logicGroup.update()
+      
+    display.fill([25, 25, 25])
+    visibleGroup.draw(display)
+
+    pygame.display.update()
+
+
